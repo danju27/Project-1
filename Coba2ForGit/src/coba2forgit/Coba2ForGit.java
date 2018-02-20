0c43323048116b770f63096002f56883a6f576e7
Mumuh Kustino <mumuhkustino@gmail.com>
2018-02-20 10:03:05 +0700

    Changes

--- Coba2ForGit/src/coba2forgit/Coba2ForGit.java
+++ Coba2ForGit/src/coba2forgit/Coba2ForGit.java
@@ -16,7 +16,7 @@ public class Coba2ForGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello olleH");
+        System.out.println("Hello >>Ö|Ö<< olleH");
     }
     
 }
